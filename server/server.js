d9d341da8faac911a6c9d02cd9083f43bc0059f5
adnruixxd31 <andruix93@gmail.com>
2023-08-31 17:52:55 -0600

    feat: working connectivy with db

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 require("dotenv").config();
+
+const db = require('./db')
+
+const { json } = require("express");
 const express = require("express");
+const morgan = require("morgan")
 
 const app = express();
 
-app.get("/api/v1/restaurants", (req, res) => {
+app.use(morgan("dev"))
+app.use(express.json())
+
+app.get("/api/v1/restaurants", async (req, res) => {
+    const results = await db.query("SELECT * FROM restaurants")
     res.status(200).json({
         status: "success",
+        results: results.rows.length,
         data: {
-            restaurants: "calabacitas tiernas"
+            restaurants: results.rows
         }
     })
 });
@@ -25,10 +35,12 @@ app.post("/api/v1/restaurants", (req, res) => {
 
 app.patch("/api/v1/restaurants/:id", (req, res) => {
     console.log(req.body)
+    res.status(200).json(req.body)
 });
 
 app.delete("/api/v1/restaurants/:id", (req, res) => {
     console.log(req.body)
+    res.status(200).json(req.body)
 });
 
 const port = process.env.PORT || 3001;
