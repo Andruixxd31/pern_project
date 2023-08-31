a1864e6fe985b9ea08cc88f5124ed2e23f261d1c
adnruixxd31 <andruix93@gmail.com>
2023-08-31 16:17:43 -0600

    feat: declaration of crud endpoints

--- server/server.js
+++ server/server.js
@@ -1,7 +1,37 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-app.listen(3007, () => {
-    console.log("Server listening at port 3007");
-})
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurants: "calabacitas tiernas"
+        }
+    })
+});
+
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params)
+    res.status(200).json(req.params["id"])
+});
+
+
+app.post("/api/v1/restaurants", (req, res) => {
+    console.log(req.body)
+    res.status(200).json(req.body)
+});
+
+app.patch("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.body)
+});
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.body)
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log("Server listening at port ", port);
+});
