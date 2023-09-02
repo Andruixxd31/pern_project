8804896195cd7a2ee80a459cde1869e6874351c2
adnruixxd31 <andruix93@gmail.com>
2023-09-01 19:29:38 -0600

    feat: add row functionality in frontend

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
 
 const AddReview = () => {
+    const { addRestaurants } = useContext(RestaurantsContext)
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("Price Range");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+              const response = await RestaurantFinder.post("/", {
+                name: name,
+                location: location,
+                price_range: priceRange,
+              });
+              console.log(response.data.data.restaurant);
+              addRestaurants(response.data.data.restaurant);
+            } catch (err) {
+              console.log(err);
+        }
+    };
   return (
     <div className="mb-4">
       <form action="">
         <div className="form-row">
           <div className="form-group col">
             <input
+              value={name}
+              onChange={e => setName(e.target.value)}
               id="name"
               placeholder="name"
               type="text"
@@ -15,30 +38,33 @@ const AddReview = () => {
           </div>
           <div className="form-group col">
             <input
+              value={location}
+              onChange={e => setLocation(e.target.value)}
               id="location"
               placeholder="location"
               type="text"
               className="form-control"
             />
           </div>
-          <div className="form-group col">
-            <label htmlFor="rating">Rating</label>
+          <div className="col">
             <select
-              id="rating"
-              className="custom-select"
+              value={priceRange}
+              onChange={(e) => setPriceRange(e.target.value)}
+              className="custom-select my-1 mr-sm-2"
             >
-              <option disabled>Rating</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
+              <option disabled>Price Range</option>
+              <option value="1">$</option>
+              <option value="2">$$</option>
+              <option value="3">$$$</option>
+              <option value="4">$$$$</option>
+              <option value="5">$$$$$</option>
             </select>
+           </div>
           </div>
-        </div>
         <button
           type="submit"
           className="btn btn-primary"
+          onClick={handleSubmit}
         >
           Submit
         </button>
